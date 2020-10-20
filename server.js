36d3a305d2a170500f349225e9c4b902618bee50
kent28808 <kktanoue@gmail.com>
2020-10-19 23:49:32 -0700

    updated server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const mysql = require("mysql");
+const ctable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3000,
+    user "root",
+    password: "password",
+    database: "Employee_Tracker"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    runSearch();
+});
+
+//Function for inquirer prompt
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?"
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Add Department",
+                "Add Role",
+                "remove employee",
+                "Update employee role",
+                "Update employee manager"
+            ]
+        }).then(answers => {
+            switch (answers.action) {
+                case "View all employees":
+                    Employees();
+                    runSearch();
+                    break;
+                case "View all employees by department"
+            }
+        })
+
+}
