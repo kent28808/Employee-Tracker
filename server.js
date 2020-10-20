ebdc4647d37091d364887a76f807ca530dd20d8a
kent28808 <kktanoue@gmail.com>
2020-10-20 15:33:44 -0700

    updated js and sql files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const mysql = require("mysql");
 const ctable = require("console.table");
+const data = require("/seed.sql");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -24,31 +25,69 @@ function runSearch() {
             type: "list",
             message: "What would you like to do?"
             choices: [
-                "View all employees",
-                "View all employees by department",
-                "View all employees by manager",
-                "Add employee",
+                "View all Employees",
+                "View all Departments",
+                "View all Roles",
+                "Add Employee",
                 "Add Department",
                 "Add Role",
-                "remove employee",
-                "Update employee role",
-                "Update employee manager"
+                "Update Employee Role",
+                "Update Employee Manager"
+                "View Employees by Manager"
+                "Remove Department"
+                "Remove Role"
+                "Remove Employee",
+                "Quit"
             ]
         }).then(answers => {
             switch (answers.action) {
                 case "View all employees":
-                    employees();
-                    runSearch();
+                    viewEmployees();
                     break;
-                case "View all employees by department":
-                    department();
-                    runSearch();
+                case "View all departments":
+                    viewDepartment();
                     break;
-                case "View all employees by manager":
-                    manager();
-                    runSearch();
-
+                case "View all roles":
+                    viewRoles();
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Add Department":
+                    viewDepartment();
+                    break;
+                case "Add Role":
+                    addRole();
+                case "Update Employee Role":
+                    updateEmployee();
+                    break;
+                case "Update Employee Manager":
+                    updateManager();
+                    break;
+                case "View Employees by Manager":
+                    viewManager();
+                case "Remove Department":
+                    removeDepartment();
+                    break;
+                case "Remove Role":
+                    removeRole();
+                    break;
+                 case "Remove Employee":
+                    removeEmployee();
+        
             }
         })
 
 }
+
+function viewEmployees()
+
+function viewDepartment()
+
+function viewRoles()
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
